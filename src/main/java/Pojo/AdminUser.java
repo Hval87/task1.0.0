d365eaa86df28b77ceaf0b6a9179d57da1ae0b94
Hval <Hval2006@ya.ru>
2024-09-03 21:47:31 +0300

    evening 1

---
+++ src/main/java/Pojo/AdminUser.java
@@ -0,0 +1,6 @@
+package Pojo;
+
+public class AdminUser extends User {
+    private int id;
+    private String name;
+}
---
+++ src/test/java/UserCompare.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class UserCompare {
+  @Test
+    public void test(){
+
+
+    }
+}
